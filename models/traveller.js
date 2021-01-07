39171e24edf6f5550d46fd10ce80dc166d1d82c0
Bela <belahamid@gmail.com>
2021-01-07 22:14:04 +0000

    test passed for calculateTotalDistanceTravelled

--- models/traveller.js
+++ models/traveller.js
@@ -33,7 +33,10 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const totalDistance = this.journeys.reduce((acc, journey) => {
+    return acc + journey.distance
+  }, 0);
+  return totalDistance;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
